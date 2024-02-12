091bd5b0845582b78d4aa61af7ba09b6e4c4375c
Magnus Fernandes <magnusfernandes1295@hotmail.com>
2024-02-12 17:18:43 +0530

    Get MQTT data from device

---
+++ go.mod
@@ -0,0 +1,11 @@
+module iisc-go
+
+go 1.21.6
+
+require github.com/eclipse/paho.mqtt.golang v1.4.3
+
+require (
+	github.com/gorilla/websocket v1.5.1 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+var knt int
+var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
+	fmt.Printf("MSG: %s\n", msg.Payload())
+	text := fmt.Sprintf("this is result msg #%d!", knt)
+	knt++
+	token := client.Publish("nn/result", 0, false, text)
+	token.Wait()
+}
+
+func main() {
+	knt = 0
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	opts := MQTT.NewClientOptions().AddBroker("tcp://101.53.142.45:1883")
+
+	opts.SetClientID("magnus-mbp")
+	opts.SetDefaultPublishHandler(f)
+	topic := "iisc/ehm/#"
+
+	opts.OnConnect = func(c MQTT.Client) {
+		if token := c.Subscribe(topic, 0, f); token.Wait() && token.Error() != nil {
+			panic(token.Error())
+		}
+	}
+	client := MQTT.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	} else {
+		fmt.Printf("Connected to server\n")
+	}
+	<-c
+}
